1b1cec329f3c77ba0089df5cb3d8c3307ab10eb5
Jose Fereira <jffcastillo0@gmail.com>
2026-06-26 20:45:04 -0400

    Fix:Se corrigio el error que no se importaban las imagenes

--- src/app/components/HoverReveal/HoverReveal.ts
+++ src/app/components/HoverReveal/HoverReveal.ts
@@ -3,26 +3,22 @@ import { Component, Input, HostBinding } from '@angular/core';
 @Component({
   selector: 'app-hover-reveal',
   standalone: true,
-  template: '<ng-content></ng-content>',
+  template: `
+    <div
+      class="hover-reveal-bg"
+      [style.background-image]="coverImage ? 'url(' + coverImage + ')' : 'none'"
+    ></div>
+    <div class="hover-reveal-overlay"></div>
+    <ng-content></ng-content>
+  `,
   styleUrl: './HoverReveal.css',
 })
 export class HoverRevealComponent {
   @Input({ required: true }) coverImage = '';
   @Input() disabled = false;
-  @Input() cornerColor = '#00f3ff';
 
   @HostBinding('attr.data-disabled')
   get disabledAttr() {
     return this.disabled ? '' : null;
-  }
-
-  @HostBinding('style.--cover-img')
-  get coverImgVar() {
-    return this.coverImage ? `url(${this.coverImage})` : 'none';
-  }
-
-  @HostBinding('style.--corner-color')
-  get cornerColorVar() {
-    return this.cornerColor;
   }
 }
